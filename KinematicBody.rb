82141eaecc81aa734ff64db483137d0679478de6
TheRealSavi <JohnGibbons167@gmail.com>
2020-04-12 09:31:59 -0400

    tons of reworked physics

--- KinematicBody.rb
+++ KinematicBody.rb
@@ -1,18 +1,28 @@
 require "matrix" #this is the builtin ruby library for vectors
 class KinematicBody
   attr_reader :touchingGround
-  attr_accessor :sticky, :mass
-  def initialize(x,y,mass,color)
-    @size = 50 #size of the square
+  attr_accessor :mass, :model
+  def initialize(x,y,color,x2, material)
+    #these are the pretty labels
+    if x2 != false
+      @label = {
+        :position => Text.new("s", x: x2, y: 30),
+        :velocity => Text.new("v", x: x2, y: 60),
+        :acceleration => Text.new("a", x: x2, y: 90),
+        :grounded => Text.new("g", x: x2, y: 120),
+        :touchingGround => Text.new("t", x: x2, y: 150)
+      }
+    else
+      @label = false
+    end
+
+    @material = material
+
     @color = color #color of the square
 
-    @timeStep = 0.6 #basically the speed of the physics
-    @sticky = false #makes you sticky
     @grounded = false #true when on the ground and not moving vertically. disables gravity.
     @touchingGround = false #true when touching ground. used for friction.
-    @maxSpeed = 200 #max velocity possible to travel
-    @mass = mass #mass of the dude
-    @bounciness = 0.8 #bounciness factor. 1 = 100%
+
     @position = Vector[x, y]
     @velocity = Vector[0,0]
     @acceleration = Vector[0,0]
@@ -21,53 +31,44 @@ class KinematicBody
     @model = Square.new(
       color: @color,
       x: @position[0], y: @position[1],
-      size: @size
+      size: @material.size
     )
   end
 
   #this is how you move him, its like giving it a shove. you just tell it how hard to move.
   def applyForce(vector)
     vector = vector.clone
-    force = vector / @mass
+    force = vector / @material.mass
     @grounded = false         #just use this opportunity to check if it really is grounded
     @acceleration += force   #adds the force to this frames acceleration
   end
 
   def gravity() #calculates and returns the force of gravity on the player
-    g = 2.0     #gravity constant
-    forceDown = @mass * g
+    g = Environment::Gravity     #gravity constant
+    forceDown = @material.mass * g
     return Vector[0, forceDown]
   end
 
   def friction() #calculates and returns the force of friction on the player
-    if @touchingGround #determines the coefficent of friction based on if its in air or on ground
+    coef = Environment::GroundFriction
 
-      if @sticky #determines how much it sticks to the ground
-        coef = 0.88 #sticky ground
-      else
-        coef = 0.22 #not sticky ground
-      end
+    v = Vector[@velocity[0], 0] #velocity vector with no y component
+    u = Vector[(v[0] / (v.r + 0.01)), (v[1] / (v.r + 0.01))] #velocity vector with no magnitude, only direction
+    u *= -1 #reverse direction so it opposes
+    u *= coef #multiply by the coefficent of friction to get the magnitude
+    u *= @material.mass * Environment::Gravity #multiply by the Normal force to increase the magnitude.
 
-      v = Vector[@velocity[0], 0]
-      u = Vector[(v[0] / (v.r + 0.01)), (v[1] / (v.r + 0.01))]
-      u *= -1
-      u *= coef
-      u *= @mass * 2.0 #Weight from gravity
-
-      if u[0] > 0.0 && u[0] < 0.05 || u[0] < 0.0 && u[0] > -0.05
-        u[0] = 0
-      end
-
-      return u
-    else
-      return Vector[0,0]
+    if u[0] > 0.0 && u[0] < 0.05 || u[0] < 0.0 && u[0] > -0.05 #get rid of the friction component if its really small
+      u[0] = 0
     end
+
+    return u
   end
 
   def drag()
-    density = 0.02
-    surfaceArea = 1
-    coef = 0.1
+    density = Environment::AirDensity
+    surfaceArea = @material.surfaceArea
+    coef = Environment::CoefficentOfDrag
 
     velocity = Vector[@velocity[0], @velocity[1]]
 
@@ -98,35 +99,32 @@ class KinematicBody
     if !@grounded
       applyForce(gravity()) #applies force of gravity unless its grounded. this stops infinte little jittering.
     end
-    applyForce(friction()) #applies force of friction
+    if @touchingGround
+      applyForce(friction()) #applies force of friction
+    end
     applyForce(drag())
 
-    @velocity += @acceleration * @timeStep #calculates additional velocity with this frames determined acceleration vector
+    @velocity += @acceleration * Environment::TimeStep #calculates additional velocity with this frames determined acceleration vector
 
     #these make sure the velocity is within range of max speed
-    @velocity[0] = @velocity[0].clamp(-@maxSpeed, @maxSpeed)
-    @velocity[1] = @velocity[1].clamp(-@maxSpeed, @maxSpeed)
+    @velocity[0] = @velocity[0].clamp(-@material.maxSpeed, @material.maxSpeed)
+    @velocity[1] = @velocity[1].clamp(-@material.maxSpeed, @material.maxSpeed)
 
-    if @velocity[0] > 0.0 && @velocity[0] < 0.52 || @velocity[0] < 0.0 && @velocity[0] > -0.52
+    if @velocity[0] > 0.0 && @velocity[0] < Environment::TimeStep || @velocity[0] < 0.0 && @velocity[0] > -Environment::TimeStep
       @velocity[0] = 0
     end
 
     #calculates addition to the potition based on the final velocity
     prevPosition = Vector[@position[0],@position[1]]
 
-    @position += @velocity * @timeStep
+    @position += @velocity * Environment::TimeStep
 
-    if @position[1] >= Window.height - 100
+    if @position[1] >= (Window.height - 50) - @material.size
       @touchingGround = true
-      @position[1] = Window.height - 100
-      if @sticky == true  #if its sticky when it touches the ground
-        #@velocity[1] *= -@bounciness * 0.2 #if its sticky reverse the velocity and scale it by the 20% bounce factor
-        @velocity[1] *= -1 * @bounciness * 0.2
-      else
-        #@velocity[1] *= -@bounciness #if its not sticky reverse the velocity and scale it by the bounce factor
-        @velocity[1] *= -1 * @bounciness
-      end
-      if @velocity[1] > 0.0 && @velocity[1] < 0.6 || @velocity[1] < 0.0 && @velocity[1] > -0.6
+      @position[1] = (Window.height - 50) - @material.size
+      @velocity[1] *= -1 * @material.bounciness
+
+      if @velocity[1] > 0.0 && @velocity[1] < Environment::TimeStep || @velocity[1] < 0.0 && @velocity[1] > -Environment::TimeStep
         @velocity[1] = 0
       end
       if @velocity == Vector[0,0]
@@ -139,16 +137,18 @@ class KinematicBody
     edgeLoop() #runs the edgeloop that handles teleporting it around the edge
     @model.x = @position[0] #updating the viewmodels position so we can see him go
     @model.y = @position[1]
+    @model.size = @material.size
 
     #these are just all the labels and formatting the strings so they look pretty
-    @label[:grounded].text = "Grounded:" + @grounded.to_s
-    @label[:touchingGround].text = "Touching Ground:" + @touchingGround.to_s
-    @label[:sticky].text = "Sticky:" + @sticky.to_s
+    if @label != false
+      @label[:grounded].text = "Grounded:" + @grounded.to_s
+      @label[:touchingGround].text = "Touching Ground:" + @touchingGround.to_s
 
-    @label[:position].text = "Position:" + ('%-8.8s' % @position[0].round(2).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @position[1].round(2).to_f).gsub(' ', '0')
-    @label[:velocity].text = "Velocity:" + ('%-8.8s' % @velocity[0].round(2).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @velocity[1].round(2).to_f).gsub(' ', '0')
-    @label[:acceleration].text = "Acceleration:" + ('%-8.8s' % @acceleration[0].round(2).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @acceleration[1].round(2).to_f).gsub(' ', '0')
-
+      @label[:position].text = "Position:" + ('%-8.8s' % @position[0].round(8).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @position[1].round(8).to_f).gsub(' ', '0')
+      @label[:velocity].text = "Velocity:" + ('%-8.8s' % @velocity[0].round(8).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @velocity[1].round(8).to_f).gsub(' ', '0')
+      @label[:acceleration].text = "Acceleration:" + ('%-8.8s' % @acceleration[0].round(8).to_f).gsub(' ', '0') + "," + ('%-8.8s' % @acceleration[1].round(8).to_f).gsub(' ', '0')
+    end
+    
     @acceleration = Vector[0,0] #erase this frames acceleration so that we have a nice fresh start next frame.
   end
 end
