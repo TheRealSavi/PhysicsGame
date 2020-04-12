44015d5950808bce1217acb7107fbafe4cd54afc
TheRealSavi <JohnGibbons167@gmail.com>
2020-04-12 09:31:03 -0400

    added controller and environment constants

---
+++ Controller.rb
@@ -0,0 +1,6 @@
+module Controller
+  Left = 10
+  Right = 10
+  Up = 30
+  Down = 20000
+end
---
+++ Environment.rb
@@ -0,0 +1,7 @@
+module Environment
+  Gravity = 2.8
+  GroundFriction = 0.32
+  AirDensity = 0.04
+  TimeStep = 0.65
+  CoefficentOfDrag = 0.16
+end
