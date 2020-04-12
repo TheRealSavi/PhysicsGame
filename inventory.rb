b7cf949ee93de7dff915c52439ea37b62ce8a6a4
TheRealSavi <JohnGibbons167@gmail.com>
2020-04-12 11:15:57 -0400

    added icons

--- inventory.rb
+++ inventory.rb
@@ -2,7 +2,7 @@ class Inventory
   attr_reader :items
   def initialize()
     @z = 1000
-    @size = 300
+    @size = 315
     @x = Window.width / 2 - @size / 2
     @y = Window.height / 2 - @size /2
     @ui = Square.new(x: @x, y: @y, z: @z, size: @size, color: 'white')
@@ -11,6 +11,7 @@ class Inventory
 
     @items = []
     @displayItems = []
+    @displayIcons = []
   end
 
   def toggleShown()
@@ -31,7 +32,8 @@ class Inventory
       @displayItems.each do |j|
         lengthOfItems += j.width
       end
-      @displayItems.push(Text.new(item.name, x: @x + 15 + lengthOfItems + (15 * @displayItems.count), y: @y + 15, z: @z +1, color: 'black'))
+      @displayItems.push(Text.new(item.name, x: @x + 15 + lengthOfItems + (15 * @displayItems.count), y: @y + 30, z: @z +1, color: 'black'))
+      @displayIcons.push(Square.new(x: @x + 15 + lengthOfItems + (15 * (@displayItems.count - 1)) + (@displayItems[i].width / 2), y: @y + 15, z: @z + 2, size: item.model.size, color: item.model.color))
     end
   end
 
@@ -40,7 +42,11 @@ class Inventory
     @displayItems.each_with_index do |item, i|
       item.remove
     end
+    @displayIcons.each_with_index do |icon, i|
+      icon.remove
+    end
     @displayItems.clear
+    @displayIcons.clear
   end
 
   def addItem(item)
