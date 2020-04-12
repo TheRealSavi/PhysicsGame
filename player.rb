fd06f221a87d7469b8ec5c53b401f1bb314b19a1
TheRealSavi <JohnGibbons167@gmail.com>
2020-04-12 09:30:45 -0400

    moved label to kinematicbody

--- player.rb
+++ player.rb
@@ -1,17 +1,7 @@
 class Player < KinematicBody
-  attr_accessor :mover
-  def initialize(x,y,mass,color,x2)
-
-    #these are the pretty labels
-    @label = {
-      :position => Text.new("s", x: x2, y: 30),
-      :velocity => Text.new("v", x: x2, y: 60),
-      :acceleration => Text.new("a", x: x2, y: 90),
-      :grounded => Text.new("g", x: x2, y: 120),
-      :sticky => Text.new("sticky", x: x2, y: 150),
-      :touchingGround => Text.new("t", x: x2, y: 180)
-    }
-    super(x,y,mass,color)
+  attr_accessor :inventory
+  def initialize(x,y,color,x2)
+    super(x,y,color,x2, Materials::PLAYER)
 
     @inventory = Inventory.new() #players inventory
   end
